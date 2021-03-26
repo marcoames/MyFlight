5bda89ca5f57c467c7aee0552cfdce1d51c6aac6
marco <marco.ames@edu.pucrs.br>
2021-03-26 18:58:53 -0300

    Versão Inicial - 2

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -4,6 +4,6 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("MyFlight project...");
-		System.out.println("Teste...");
+		System.out.println("Teste....");
 	}
 }
